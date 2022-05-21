8b2671962382a0899ecb729fb98d538fa69aaaf2
tesddev <tesleem.amuda@gmail.com>
2022-05-21 15:50:14 +0100

    add pod file with rxswift and rococo

---
+++ RxSwiftPractice/RxSwiftPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftPractice
+//
+//  Created by GIGL iOS on 21/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
