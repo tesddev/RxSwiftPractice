f31e8d30d198c47152cf74a056a83c88d8b01b0f
tesddev <tesleem.amuda@gmail.com>
2022-05-21 16:01:09 +0100

    complete the fetch item method

--- RxSwiftPractice/RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/RxSwiftPractice/ViewController.swift
@@ -18,7 +18,16 @@ struct ProductViewModel {
     var items = PublishSubject<[Product]>()
     
     func fetchItems() {
+        let products = [
+            Product(imageName: "house", imageTitle: "Home"),
+            Product(imageName: "gear", imageTitle: "Settings"),
+            Product(imageName: "person.circle", imageTitle: "Profile"),
+            Product(imageName: "airplane ", imageTitle: "Flights"),
+            Product(imageName: "bell", imageTitle: "Notification")
+        ]
         
+        items.onNext(products)
+        items.onCompleted()
     }
 }
 
