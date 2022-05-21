26e3b8f6801aa880951120508681105bec293c33
tesddev <tesleem.amuda@gmail.com>
2022-05-21 16:06:58 +0100

    create table view programmatically

--- RxSwiftPractice/RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/RxSwiftPractice/ViewController.swift
@@ -32,6 +32,12 @@ struct ProductViewModel {
 }
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
